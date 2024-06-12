d231db61c791d62d0d0cb4785fee4b4d0c2168c9
rayanjainn <rayansjain@gmail.com>
2024-06-12 19:29:07 +0530

    Cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -75,6 +75,12 @@ document.querySelectorAll(".add-to-cart-button").forEach((button) => {
         quantity: 1,
       });
     }
-    console.log(cart);
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".cart-quantity").innerHTML = cartQuantity;
   });
 });
